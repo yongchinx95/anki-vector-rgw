95f96908d78d58019870ec20ec7b7559cce1aa8c
Kevin Schrader <kevin.schrader@student.ehb.be>
2020-03-09 16:45:55 +0100

    update

--- scripts/test.py
+++ scripts/test.py
@@ -9,7 +9,7 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.abspath('resources/small_dog.jpg')
+file_name = os.path.abspath('small_dog.jpg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
