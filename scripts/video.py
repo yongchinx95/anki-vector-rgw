545d246d3211999cab9b3a893d51bc8cb33682fb
Kevin Schrader <kevin.schrader@student.ehb.be>
2020-03-10 11:58:31 +0100

    audio

--- scripts/video.py
+++ scripts/video.py
@@ -1,19 +1,3 @@
-# import anki_vector
-#
-#
-# def main():
-#     args = anki_vector.util.parse_command_args()
-#     with anki_vector.Robot(args.serial) as robot:
-#
-#         robot.audio.stream_wav_file("../resources/music/petitbiscuit.wav", 75)
-#
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
