4a1db391429ccd31944c99ef980d8c4069bb1c91
Kevin Schrader <kevin.schrader@student.ehb.be>
2020-03-11 15:13:36 +0100

    update

--- scripts/rocketchatApi.py
+++ scripts/rocketchatApi.py
@@ -1,4 +1,16 @@
+from pprint import pprint
 from rocketchat.api import RocketChatAPI
+from rocketchat_API.rocketchat import RocketChat
 
 api = RocketChatAPI(settings={'username': 'Kevin', 'password': 'Rgwit,912',
                               'domain': 'https://chat.rgwit.be'})
+#get information
+api.get_my_info()
+
+#send message to room
+api.send_message('test', 'GENERAL')
+
+#send dm
+api.send_message('test', '@yongchin')
+
+#
