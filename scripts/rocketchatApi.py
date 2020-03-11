905d03311f294630f6631a7647adf6463d2a88dc
Kevin Schrader <kevin.schrader@student.ehb.be>
2020-03-11 12:12:23 +0100

    rocketchat api

---
+++ scripts/rocketchatApi.py
@@ -0,0 +1,4 @@
+from rocketchat.api import RocketChatAPI
+
+api = RocketChatAPI(settings={'username': 'Kevin', 'password': 'Rgwit,912',
+                              'domain': 'https://chat.rgwit.be'})
